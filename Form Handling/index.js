01b4b1780132a4f27195584dc99788142e444de0
mdrezuanislamridoy <mdrezuanislamridoy@gmail.com>
2024-09-13 16:01:45 +0600

    dfd

--- Form Handling/index.js
+++ Form Handling/index.js
@@ -7,15 +7,27 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get("/register", (req, res) => {
+app.get("/home", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/register", (req, res) => {
+app.get("/triangle", (req, res) => {
+  res.sendFile(__dirname + "/triangle.html");
+});
+app.get("/circle", (req, res) => {
+  res.sendFile(__dirname + "/circle.html");
+});
+
+app.post("/triangle", (req, res) => {
   const one = req.body.one;
   const two = req.body.two;
-  res.send(`<h1>calculation is  ${parseInt(one) + parseInt(two)}</h1>`);
+  res.send(`area of triangle is ${one * two}`);
 });
+app.post("/circle", (req, res) => {
+  const one = req.body.one;
+  res.send(`area of circle is ${3.1416 * one * one}`);
+});
+
 app.listen(port, () => {
   console.log("port is ", port);
 });
